663e1995f15abecd90b53e64a320954f14bd5001
Luc191 <lucifer-191@mail.ru>
2024-03-28 11:57:19 +0300

    Lab1

---
+++ Lab_1_3/Program.cs
@@ -0,0 +1,34 @@
+﻿string[] myarray = new string[20];
+int id = 0;
+
+Tree begin = new Tree("Животные");
+Tree node1 = new Tree("Бесчерепные");
+Tree node2 = new Tree("Оболочники");
+Tree node3 = new Tree("Позвоночные");
+Tree node4 = new Tree("Ланцетники");
+Tree node5 = new Tree("Аппендикулярии");
+Tree node6 = new Tree("Асцидии");
+Tree node7 = new Tree("Сальпы");
+Tree node8 = new Tree("Бореоэутерии");
+Tree node9 = new Tree("Эуархонтоглиры");
+Tree node10 = new Tree("Лавразиатерии");
+
+begin.NewChild(node1);
+begin.NewChild(node2);
+begin.NewChild(node3);
+node1.NewChild(node4);
+node2.NewChild(node5);
+node2.NewChild(node6);
+node2.NewChild(node7);
+node3.NewChild(node8);
+node8.NewChild(node9);
+node8.NewChild(node10);
+
+begin.ShowTree(myarray, ref id);
+
+for (int i = 0; i < id; i++)
+{
+    Console.WriteLine(myarray[i]);
+}
+
+
---
+++ Lab_1_3/Tree.cs
@@ -0,0 +1,29 @@
+﻿public class Tree
+{
+    public List<Tree> Child { get; set; }
+    public string Value { get; set; }
+    public Tree(string value)
+    {
+        Value = value;
+        Child = new List<Tree>();
+    }
+    public void NewChild(Tree node)
+    {
+        Child.Add(node);
+    }
+    public void ShowTree(string[] myarray, ref int id)
+    {
+        if (Child.Count > 0)
+        {
+            myarray[id++] = "Потомки узла " + Value + ": ";
+            for (int i = 0; i < Child.Count; i++)
+            {
+                myarray[id++] = Child[i].Value;
+            }
+            for (int i = 0; i < Child.Count; i++)
+            {
+                Child[i].ShowTree(myarray, ref id);
+            }
+        }
+    }
+}
